5920ed4a31f5c8d580fdff1f4f21902f0b4b9b4c
donavon <github@donavon.com>
2019-01-18 09:01:06 -0500

    use react-proxy-hook to test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,9 @@
-import React, { Fragment } from 'react';
-import { render, cleanup, fireEvent } from 'react-testing-library';
+import { proxyHook, cleanup } from 'react-proxy-hook';
 import 'jest-dom/extend-expect';
 
 import useDarkMode from '../src';
+
+const useDarkModeProxy = proxyHook(useDarkMode);
 
 afterEach(cleanup);
 
@@ -19,48 +20,29 @@ const createTestElement = obj => ({
   },
 });
 
-const Component = ({ initialDarkMode, config }) => {
-  const darkMode = useDarkMode(initialDarkMode, config);
-  return (
-    <Fragment>
-      <span data-testid="isDarkMode">{JSON.stringify(darkMode.value)}</span>
-      <button data-testid="setDarkMode" type="button" onClick={darkMode.enable} />
-      <button data-testid="clearDarkMode" type="button" onClick={darkMode.disable} />
-      <button data-testid="toggleDarkMode" type="button" onClick={darkMode.toggle} />
-    </Fragment>
-  );
-};
-
 describe('useDarkMode', () => {
   test('import useDarkMode from "use-dark-mode"', () => {
-    expect(typeof useDarkMode).toBe('function');
+    expect(typeof useDarkModeProxy).toBe('function');
   });
 
   test('you can pass an `initialValue`', () => {
-    const {
-      getByTestId,
-    } = render(<Component initialDarkMode={true} config={{ element: createTestElement({}) }} />);
-    const isDarkModeElement = getByTestId('isDarkMode');
-    expect(isDarkModeElement.textContent).toBe('true');
+    const darkMode = useDarkModeProxy(true, { element: createTestElement({}) });
+    expect(darkMode.value).toBe(true);
   });
 
   test('`initialValue` defaults to `false`', () => {
-    const { getByTestId } = render(<Component config={{ element: createTestElement({}) }} />);
-    const isDarkModeElement = getByTestId('isDarkMode');
-    expect(isDarkModeElement.textContent).toBe('false');
+    const darkMode = useDarkModeProxy(undefined, { element: createTestElement({}) });
+    expect(darkMode.value).toBe(false);
   });
 
-  test('you can pass an optional `config`', () => {
-    const { getByTestId } = render(<Component />);
-    const isDarkModeElement = getByTestId('isDarkMode');
-    expect(isDarkModeElement.textContent).toBe('false');
+  test('`config` is optional', () => {
+    const darkMode = useDarkModeProxy();
+    expect(darkMode.value).toBe(false);
   });
 
   test('`config.className` is applied to the element `config.element`', (done) => {
     const test = {};
-    const {
-      getByTestId,
-    } = render(<Component initialDarkMode={true} config={{ className: 'foo', element: createTestElement(test) }} />);
+    useDarkModeProxy(true, { className: 'foo', element: createTestElement(test) });
     setTimeout(() => {
       expect(test).toEqual({ className: 'foo', method: 'add' });
       done();
@@ -72,40 +54,29 @@ describe('useDarkMode', () => {
       expect(isDarkMode).toBe(true);
       done();
     };
-    render(<Component initialDarkMode={true} config={{ onChange }} />);
+    useDarkModeProxy(true, { onChange });
   });
 
   test('you can call `darkMode.enable` to set dark mode', () => {
-    const {
-      getByTestId,
-    } = render(<Component initialDarkMode={false} config={{ element: createTestElement({}) }} />);
-    const button = getByTestId('setDarkMode');
-    fireEvent.click(button);
-    const isDarkModeElement = getByTestId('isDarkMode');
-    expect(isDarkModeElement.textContent).toBe('true');
+    const darkMode = useDarkModeProxy(false, { element: createTestElement({}) });
+    expect(darkMode.value).toBe(false);
+    darkMode.enable();
+    expect(darkMode.value).toBe(true);
   });
 
   test('you can call `darkMode.disable` to clear dark mode', () => {
-    const {
-      getByTestId,
-    } = render(<Component initialDarkMode={true} config={{ element: createTestElement({}) }} />);
-    const button = getByTestId('clearDarkMode');
-    fireEvent.click(button);
-    const isDarkModeElement = getByTestId('isDarkMode');
-    expect(isDarkModeElement.textContent).toBe('false');
+    const darkMode = useDarkModeProxy(true, { element: createTestElement({}) });
+    expect(darkMode.value).toBe(true);
+    darkMode.disable();
+    expect(darkMode.value).toBe(false);
   });
 
   test('you can call `darkMode.toggle` to toggle dark mode on/off/on', () => {
-    const {
-      getByTestId,
-    } = render(<Component initialDarkMode={true} config={{ element: createTestElement({}) }} />);
-    const button = getByTestId('toggleDarkMode');
-    const isDarkModeElement = getByTestId('isDarkMode');
-    fireEvent.click(button);
-    expect(isDarkModeElement.textContent).toBe('false');
-    fireEvent.click(button);
-    expect(isDarkModeElement.textContent).toBe('true');
-    fireEvent.click(button);
-    expect(isDarkModeElement.textContent).toBe('false');
+    const darkMode = useDarkModeProxy(true, { element: createTestElement({}) });
+    expect(darkMode.value).toBe(true);
+    darkMode.toggle();
+    expect(darkMode.value).toBe(false);
+    darkMode.toggle();
+    expect(darkMode.value).toBe(true);
   });
 });
