00d92e585792d4a7bd60e6dfb19e06a01e4a08c4
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2019-11-26 20:45:23 -0500

    Fix typos

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,19 +1,19 @@
 declare module 'use-dark-mode' {
   /**
-   * A config objet allowing you to specify certain aspects of `useDarkMode`
+   * A config object allowing you to specify certain aspects of `useDarkMode`
    */
   export interface DarkModeConfig {
     classNameDark?: string; // A className to set "dark mode". Default = "dark-mode".
     classNameLight?: string; // A className to set "light mode". Default = "light-mode".
     element?: HTMLElement; // The element to apply the className. Default = `document.body`
     onChange?: (val?: boolean) => void; // Overide the default className handler with a custom callback.
-    storageKey?: string; // Specify the `localStorage` key. Default = "darkMode". Sewt to `null` to disable persistent storage.
+    storageKey?: string; // Specify the `localStorage` key. Default = "darkMode". Set to `null` to disable persistent storage.
     storageProvider?: WindowLocalStorage; // A storage provider. Default = `localStorage`.
     global?: Window; // The global object. Default = `window`.
   }
 
   /**
-   * An object returned frm a call to `useDarkMode`.
+   * An object returned from a call to `useDarkMode`.
    */
   export interface DarkMode {
     readonly value: boolean;
